dc4fe83c631ba0c51b51b99a6f559b9614c96c9c
Roy Catimbang <roy.catimbang@multisyscorp.com>
2021-03-04 13:46:00 +0800

    Hide Testimonials, update subtitle.

--- index.php
+++ index.php
@@ -87,7 +87,7 @@
       <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
         <div class="col-xl-6 col-lg-8">
           <h1>Powerful Digital Solutions With 8PointZero<span>.</span></h1>
-          <h2>We are team of talented digital marketers</h2>
+          <h2>Hire Remote Developers and IT Staff</h2>
         </div>
       </div>
 
@@ -95,31 +95,31 @@
         <div class="col-xl-2 col-md-4">
           <div class="icon-box">
             <i class="ri-store-line"></i>
-            <h3><a href="">Lorem Ipsum</a></h3>
+            <h3>Lorem Ipsum</h3>
           </div>
         </div>
         <div class="col-xl-2 col-md-4">
           <div class="icon-box">
             <i class="ri-bar-chart-box-line"></i>
-            <h3><a href="">Dolor Sitema</a></h3>
+            <h3>Dolor Sitema</h3>
           </div>
         </div>
         <div class="col-xl-2 col-md-4">
           <div class="icon-box">
             <i class="ri-calendar-todo-line"></i>
-            <h3><a href="">Sedare Perspiciatis</a></h3>
+            <h3>Sedare Perspiciatis</h3>
           </div>
         </div>
         <div class="col-xl-2 col-md-4">
           <div class="icon-box">
             <i class="ri-paint-brush-line"></i>
-            <h3><a href="">Magni Dolores</a></h3>
+            <h3>Magni Dolores</h3>
           </div>
         </div>
         <div class="col-xl-2 col-md-4">
           <div class="icon-box">
             <i class="ri-database-2-line"></i>
-            <h3><a href="">Nemos Enimade</a></h3>
+            <h3>Nemos Enimade</h3>
           </div>
         </div>
       </div>
