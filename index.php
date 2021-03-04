29e03a891310a48a881018cd4006e6fe8d9f067a
Roy Catimbang <roy.catimbang@multisyscorp.com>
2021-03-04 14:27:30 +0800

    Update map.

--- index.php
+++ index.php
@@ -669,7 +669,7 @@
         </div>
 
         <div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d130985.14796358463!2d120.96318058871562!3d14.453224917023752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d5006c962a1f%3A0xc19e614c3002241!2sR-2%2C%20Dasmari%C3%B1as%2C%20Cavite!5e0!3m2!1sen!2sph!4v1614839000025!5m2!1sen!2sph" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+          <iframe style="border:0; width: 100%; height: 270px;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d130985.14796358463!2d120.96318058871562!3d14.453224917023752!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397d5006c962a1f%3A0xc19e614c3002241!2sR-2%2C%20Dasmari%C3%B1as%2C%20Cavite!5e0!3m2!1sen!2sph!4v1614839121998!5m2!1sen!2sph" frameborder="0" allowfullscreen></iframe>
         </div>
 
         <div class="row mt-5">
