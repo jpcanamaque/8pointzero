b237c9c702e31f26341e7c1574091488405eb975
recatimbang <recatimbang@multisyscorp.com>
2021-03-13 16:23:00 +0800

    add response json.

--- forms/contact.php
+++ forms/contact.php
@@ -1,25 +1,5 @@
 <?php
-// require '../vendor/autoload.php'; // If you're using Composer (recommended)
-
-// var_dump('done autoload');
-// $email = new \SendGrid\Mail\Mail(); 
-// $email->setFrom("8pointzerotech@gmail.com", "Eight Point Zero");
-// $email->setSubject("Sending with SendGrid is Fun");
-// $email->addTo("catimbangroy@gmail.com", "Roy Catimbang");
-// $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
-// $email->addContent(
-//     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
-// );
-// $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-// try {
-//     $response = $sendgrid->send($email);
-//     print $response->statusCode() . "\n";
-//     print_r($response->headers());
-//     print $response->body() . "\n";
-// } catch (Exception $e) {
-//     echo 'Caught exception: '. $e->getMessage() ."\n";
-// }
-
+header('Content-type: application/json');
 require '../vendor/autoload.php'; // If you're using Composer (recommended)
 
 $email = new \SendGrid\Mail\Mail(); 
