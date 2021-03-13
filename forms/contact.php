b4b348f96bf8928c9ea877f9dfada0b27287984f
recatimbang <recatimbang@multisyscorp.com>
2021-03-13 14:42:04 +0800

    Remove mailgun script.

--- forms/contact.php
+++ forms/contact.php
@@ -1,15 +1,3 @@
 <?php
-  use Mailgun\Mailgun;
-  # Instantiate the client.
-  $mgClient = new Mailgun('a4ab7c551e63346dc5f5f9e65079edcf-e49cc42c-d6faf050');
-  $domain = "sandbox8ef4824bfa764ab1b041c87dad37ee08.mailgun.org";
-  # Make the call to the client.
-  $result = $mgClient->sendMessage($domain, array(
-      'from'	=> $_POST['name'] . ' <8pointzero@gmail.com>',
-      'to'	    => '<catimbangroy@gmail.com>',
-      'subject' => $_POST['subject'],
-      'text'	=> 'Email: ' . $_POST['message'] . ' - ' . $_POST['message']
-  ));
 
-  return;
 ?>
