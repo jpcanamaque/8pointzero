320284d07d989df68392032e5b5504b52b82ee2b
recatimbang <recatimbang@multisyscorp.com>
2021-03-13 15:41:25 +0800

    Configure recipients and email message.

--- forms/contact.php
+++ forms/contact.php
@@ -6,16 +6,25 @@ $email = new \SendGrid\Mail\Mail();
 $email->setFrom("8pointzerotech@gmail.com", "Eight Point Zero");
 $email->setSubject("Sending with SendGrid is Fun");
 $email->addTo("catimbangroy@gmail.com", "Roy Catimbang");
+$email->addTo("jpcanamaque@gmail.com", "Johnroe Paulo Canamaque");
+$email->addTo("kevinjayroluna@gmail.com", "Kevin Jay Roluna");
+$email->addTo("jeffrybordeos@gmail.com", "Jeffry Bordeos");
 $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
-    "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+    "text/html", "<strong>Name</strong>: " . $_POST['name']
+);
+$email->addContent(
+  "text/html", "<strong>Email</strong>: " . $_POST['email']
+);
+$email->addContent(
+  "text/html", "<strong>Subject</strong>: " . $_POST['subject']
+);
+$email->addContent(
+  "text/html", "<strong>Message</strong>: " . $_POST['message']
 );
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
-    $response = $sendgrid->send($email);
-    print $response->statusCode() . "\n";
-    print_r($response->headers());
-    print $response->body() . "\n";
+    $sendgrid->send($email);
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
