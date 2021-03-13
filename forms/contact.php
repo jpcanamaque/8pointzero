ed323f85495eced81ac48581b13b4cecb4f753d2
recatimbang <recatimbang@multisyscorp.com>
2021-03-13 16:04:34 +0800

    refactor email content.

--- forms/contact.php
+++ forms/contact.php
@@ -24,24 +24,26 @@ require '../vendor/autoload.php'; // If you're using Composer (recommended)
 
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("8pointzerotech@gmail.com", "Eight Point Zero");
-$email->setSubject("Sending with SendGrid is Fun");
+$email->setSubject("Customer Inquiry");
 $email->addTo("catimbangroy@gmail.com", "Roy Catimbang");
 $email->addTo("jpcanamaque@gmail.com", "Johnroe Paulo Canamaque");
 $email->addTo("kevinjayroluna@gmail.com", "Kevin Jay Roluna");
 $email->addTo("jeffrybordeos@gmail.com", "Jeffry Bordeos");
-$email->addContent("text/plain", "and easy to do anywhere, even with PHP");
 $email->addContent(
-    "text/html", "<strong>Name</strong>: " . $_POST['name']
+    "text/plain", 
+    "<strong>Name</strong>: " . $_POST['name']
+        . "\r\n<strong>Email</strong>: " . $_POST['email']
+        . "\r\n<strong>Subject</strong>: " . $_POST['subject']
+        . "\r\n<strong>Message</strong>: " . $_POST['message']
 );
 $email->addContent(
-  "text/html", "<strong>Email</strong>: " . $_POST['email']
+    "text/html",
+    "<strong>Name</strong>: " . $_POST['name']
+        . "<br><strong>Email</strong>: " . $_POST['email']
+        . "<br><strong>Subject</strong>: " . $_POST['subject']
+        . "<br><strong>Message</strong>: " . $_POST['message']
 );
-$email->addContent(
-  "text/html", "<strong>Subject</strong>: " . $_POST['subject']
-);
-$email->addContent(
-  "text/html", "<strong>Message</strong>: " . $_POST['message']
-);
+
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
     $response = $sendgrid->send($email);
