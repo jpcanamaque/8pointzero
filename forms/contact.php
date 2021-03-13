6531c6be015d4376b8c087a14f45357292ab9b48
recatimbang <recatimbang@multisyscorp.com>
2021-03-13 15:46:31 +0800

    test response.

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,29 @@
 <?php
-require '../vendor/autoload.php'; // If you're using Composer (recommended)
+// require '../vendor/autoload.php'; // If you're using Composer (recommended)
 
-var_dump('done autoload');
+// var_dump('done autoload');
+// $email = new \SendGrid\Mail\Mail(); 
+// $email->setFrom("8pointzerotech@gmail.com", "Eight Point Zero");
+// $email->setSubject("Sending with SendGrid is Fun");
+// $email->addTo("catimbangroy@gmail.com", "Roy Catimbang");
+// $email->addContent("text/plain", "and easy to do anywhere, even with PHP");
+// $email->addContent(
+//     "text/html", "<strong>and easy to do anywhere, even with PHP</strong>"
+// );
+// $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
+// try {
+//     $response = $sendgrid->send($email);
+//     print $response->statusCode() . "\n";
+//     print_r($response->headers());
+//     print $response->body() . "\n";
+// } catch (Exception $e) {
+//     echo 'Caught exception: '. $e->getMessage() ."\n";
+// }
+
+// require '../vendor/autoload.php'; // If you're using Composer (recommended)
+
+// var_dump('done autoload');
+
 $email = new \SendGrid\Mail\Mail(); 
 $email->setFrom("8pointzerotech@gmail.com", "Eight Point Zero");
 $email->setSubject("Sending with SendGrid is Fun");
@@ -24,7 +46,10 @@ $email->addContent(
 );
 $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
 try {
-    $sendgrid->send($email);
+    $response = $sendgrid->send($email);
+    print $response->statusCode() . "\n";
+    print_r($response->headers());
+    print $response->body() . "\n";
 } catch (Exception $e) {
     echo 'Caught exception: '. $e->getMessage() ."\n";
 }
